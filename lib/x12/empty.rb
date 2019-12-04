31f4acb4900224910b21fe96cd74a281bbb96faa
Clay Dunston <claydunston@gmail.com>
2019-12-04 12:23:21 -0600

    [YARD]

--- lib/x12/empty.rb
+++ lib/x12/empty.rb
@@ -1,12 +1,15 @@
 module X12
-  # Class indicating the absense of any X12 element, be it loop, segment, or anything else like that.
+  # Class indicating the absense of any X12 element, be it Loop, Segment, or anything else like that.
   class Empty < Base
-    # Create a new Empty
+    # Create a new Empty.
+    #
+    # @return [void]
     def initialize
       super(nil, [])
     end
 
-    # Returns an empty string
+    # Returns an empty string.
+    #
     # @return [String]
     def to_s
       return ''
